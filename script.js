84a412cbf9995631a2e9d60ceeddb3270b16be95
aman3170 <aman3170@gmail.com>
2025-01-13 15:58:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const chatbot = new LexWebUi({
   botName: 'DailyTaskChatbot',
   LexingtonRuntime: '2018-11-15',
   AWSRegion: 'us-east-1',
-  identityPoolId: 'YOUR_IDENTITY_POOL_ID',
+  identityPoolId: 'us-east-1:176ea5af-94b9-4f0a-a5c7-9bf54e9b717d',
   ui: {
     container: document.getElementById('chatbot'),
     inputBox: 'text',
